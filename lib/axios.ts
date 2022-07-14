e5884d460fedf38085ec2fa4244bab1e0558877d
Daniel Tehrani <contact@dantehrani.com>
2022-07-14 16:49:17 +0900

    Set GATEWAY_URL

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axiosBase from "axios";
 
 const axios = axiosBase.create({
-  baseURL: process.env.STORY_URL || "http://localhost:4000"
+  baseURL: process.env.GATEWAY_URL || "http://localhost:4000"
 });
 
 export default axios;
