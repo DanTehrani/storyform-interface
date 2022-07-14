2ead93c6082be42c1ea4c8119e4921f7fc9bc9bb
DanTehrani <DanTehrani@users.noreply.github.com>
2022-07-14 08:51:38 +0000

    deploy: 1791a476ec285d28c876be8edc7782e459ed101f

--- _next/static/yDUxiFI3tzDKM4en_-kD7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f92772d9000bb14c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
