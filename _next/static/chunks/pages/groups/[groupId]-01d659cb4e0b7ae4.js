3fd1ab2d2ee8c41460139b0502da3c1a2ec7aad7
DanTehrani <DanTehrani@users.noreply.github.com>
2022-08-06 08:49:39 +0000

    deploy: dee91ed2525b7e909a2bc9d30cbfc27e90a4fe24

---
+++ _next/static/chunks/pages/groups/[groupId]-01d659cb4e0b7ae4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[622],{84789:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/groups/[groupId]",function(){return c(21836)}])},21836:function(c,a,b){"use strict";b.r(a);var d=b(85893);a.default=function(){return(0,d.jsx)(d.Fragment,{})}}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=84789)}),_N_E=a.O()}])
