1fe0ec5072a560b873a833d2d183a582498961b5
Daniel Tehrani <contact@dantehrani.com>
2022-08-06 19:51:36 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,7 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-const ALPHA_FORM_ID =
-  "bc7d1b4356f163b717582de5c3e309f880ab97c8132ef38c1630ea11a595513d";
 
 const Index: NextPage = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push(`/forms/${ALPHA_FORM_ID}`);
-  }, [router]);
-
-  return <></>;
+  return <>Welcome</>;
 };
 
 export default Index;
