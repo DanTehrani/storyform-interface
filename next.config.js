83079d4ea1b313870c84ea83407aca19f6eb9475
Daniel Tehrani <contact@dantehrani.com>
2022-08-06 19:44:59 +0900

    Switched to Vercel

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const GITHUB_PAGES_PATH =
-  // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "production" ? "/story-form-interface" : "";
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: GITHUB_PAGES_PATH,
-
-  assetPrefix: GITHUB_PAGES_PATH,
   webpack5: true,
   webpack: config => {
     config.resolve.fallback = { fs: false };
