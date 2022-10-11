62101888a8a259bbfebcff5a5c2139b4fea695ba
Daniel Tehrani <contact@dantehrani.com>
2022-10-11 15:13:36 +0900

    Add wasm point pre-computer

---
+++ lib/wasmPreCompute/wasm/getPreComputes.wasm.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function compute_powers(a: number, b: number, c: number): void;
+export function hello_wasm(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_free(a: number, b: number): void;
---
+++ lib/wasmPreCompute/wasmPreCompute.ts
@@ -0,0 +1,18 @@
+import initWasm from "./wasm/getPreComputes";
+import { compute_powers } from "./wasm/getPreComputes";
+import { PointPreComputes } from "../../types";
+
+export const getPointPreComputes = async (
+  pointHex: string
+): Promise<PointPreComputes> => {
+  await initWasm();
+
+  const result = compute_powers(pointHex);
+  const preComputes: PointPreComputes = JSON.parse(result).powers.map(limbs =>
+    limbs.map(coordinates =>
+      coordinates.map(registers => registers.map(register => BigInt(register)))
+    )
+  );
+
+  return preComputes;
+};
